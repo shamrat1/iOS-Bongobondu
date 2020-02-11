f8bad2da54b75ea6467186026380a5dc45166202
Tanvir Ahmed <tanvir841.ta@gmail.com>
2020-02-11 11:46:04 +0600

    dew pages

--- iOS-Bongobondu/SceneDelegate.swift
+++ iOS-Bongobondu/SceneDelegate.swift
@@ -14,9 +14,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        UserDefaults.standard.set("tanvir", forKey: "developer")
+        if UserDefaults.standard.string(forKey: "developer") == "tanvir" {
+            let storyboard = UIStoryboard(name: "SecondMain", bundle: nil)
+            let vc = storyboard.instantiateViewController(identifier: "secondView") as? SecondViewController
+            window?.rootViewController = vc
+            window?.makeKeyAndVisible()
+        }
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
